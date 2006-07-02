0dbbae92c7bccd672442f6abf0016de4a13922ed
sky <sky@011c6a6d-750f-0410-a5f6-93fdcd050bc4>
2006-07-02 06:52:11 +0000

    turn the examples into a test

--- trunk/api/perl/Gearman-Client-Async/t/worker.pl
+++ trunk/api/perl/Gearman-Client-Async/t/worker.pl
@@ -4,7 +4,7 @@ use strict;
 use lib "$ENV{HOME}/cvs/wcmtools/gearman/lib";
 use Gearman::Worker;
 my $worker = Gearman::Worker->new;
-$worker->job_servers('127.0.0.1');
+$worker->job_servers('127.0.0.1:9000', '127.0.0.1:9001');
 
 $worker->register_function("sleep_for" => sub {
     my $job = shift;
@@ -13,8 +13,7 @@ $worker->register_function("sleep_for" => sub {
     my $res = rand();
     $job->set_status(0, $arg);
     for my $i (1..$arg) {
-        sleep 1;
-        warn "$$ working on $res: $i/$arg\n";
+        select(undef,undef,undef,0.25);
         $job->set_status($i, $arg);
     }
     return $res;
